9c507af95dc1c41213f25a91d2d4739bf81fa3c9
quality <quality@cdps-MacBook-Pro.local>
2019-05-09 19:03:09 +0530

    project new

---
+++ src/main/java/com/qk/axis/AxisBankMobileAutomation/App.java
@@ -0,0 +1,14 @@
+package com.qk.axis.AxisBankMobileAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+import org.testng.annotations.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
